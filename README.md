ab9a9e653c6e3032fe9219cac088932f27214bed
Diego Martin <martind@vitessepsp.com>
2024-07-03 09:06:24 +0200

    Sample dotnet tool

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # coderiff-toolkit
 Dotnet Tool
+
+Install globally
+```
+dotnet tool install -g Coderiff.Toolkit
+```
---
+++ src/Coderiff.Toolkit/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+if (args.Length == 0)
+{
+    var versionString = Assembly.GetEntryAssembly()?
+        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+        .InformationalVersion;
+
+    Console.WriteLine($"coderiff v{versionString}");
+    Console.WriteLine("-------------");
+    Console.WriteLine("\nUsage:");
+    Console.WriteLine("  coderiff <command>");
+    return;
+}
+
+Console.WriteLine(string.Join(' ', args));
+Console.WriteLine("TODO");
